34f193f599c980f478a5145fd75ce897b4e1526e
Xenofon Golfinopoulos <v-xgolfinopoulos@eurobank.gr>
2018-04-01 15:52:39 +0300

    some basic css

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
-function onExecuted(result) {
-}
+const parent = document.getElementById("container");
 
-function onError(error) {
-}
-
-function myFunc(e) {
-  browser.tabs.executeScript({
-    file: "/content_scripts/shakira.js"
-  });
-}
-document.getElementById("btn").addEventListener('click', myFunc);
-
-let parent = document.getElementById("myList");
-
-const onCaptured = (imageUri, node) => {
-  var image = document.createElement('IMG');
+const onCaptured = (imageUri, tabItem) => {
+  const image = document.createElement('img');
   image.setAttribute('src', imageUri);
-  image.style.height = '200px';
-  image.style.width = '200px';
-  node.appendChild(image);
+  image.setAttribute('class', 'image');
+  tabItem.appendChild(image);
 };
 
-function logTabs(tabs) {
-  for (let tab of tabs) {
-    console.log(tab.id);
-    let node = document.createElement("LI");
-    let textnode = document.createTextNode(tab.url);
-    node.appendChild(textnode);
-    parent.appendChild(node);
+const createTabItems = (tabs) => {
+  tabs.forEach((tab) => {
+    const tabItem = document.createElement("div");
+    tabItem.setAttribute('class', 'grid-item');
+   /* const textNode = document.createTextNode(tab.url);
+    node.appendChild(textNode);*/
     const capturing = browser.tabs.captureTab(tab.id);
-    capturing.then((uri) =>{ onCaptured(uri, node); });
-  }
+    capturing.then((uri) =>{ onCaptured(uri, tabItem); });
+
+    parent.appendChild(tabItem);
+  })
+  ;
 }
 
-function getTabs() {
-  var querying = browser.tabs.query({});
-  querying.then(logTabs);
+const getTabs = () => {
+  const querying = browser.tabs.query({});
+  querying.then(createTabItems);
 }
 
 getTabs();
